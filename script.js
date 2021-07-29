dd29598dc93a8e206ba519aa92da4ee3e6ed6efb
lionpowder <hanna7810@hotmail.com>
2021-07-29 13:12:40 -0400

    3 files modified

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-console.log('hello!')
+console.log('hello!');
 
 window.FontAwesomeConfig = {
   searchPseudoElements: true
+};
+
+function profileHeaderOnclick() {
+  var profileHeaderButton = document.getElementById(
+    'UI_Mixins__TooltipMixin_16'
+  );
+  var profileContainer = document.getElementsByClassName('UserActionsMenu')[0];
+  var isOpen = !(profileHeaderButton.ariaExpanded === 'true');
+
+  profileHeaderButton.ariaExpanded = isOpen;
+
+  profileHeaderButton.setAttribute('popupactive', isOpen);
+
+  profileContainer.style.visibility = isOpen ? 'visible' : 'hidden';
 }
